68dd5728a69d440f068edbe7e746ab07b10d2a2d
Deepti Nigam <nigam.d@husky.neu.edu>
2019-04-12 19:26:05 -0400

    Adding the template driven validations to payment form

--- BookMyMovie/src/app/app.component.ts
+++ BookMyMovie/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BookMyMovie';
+ // data: any = {};
+
+ /* onSubmit()
+  {
+    alert(JSON.stringify(this.data))
+  }*/
 }
--- BookMyMovie/src/app/app.module.ts
+++ BookMyMovie/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+    //AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
