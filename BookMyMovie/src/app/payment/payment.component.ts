6e5209ae6529509915643ebcc450171b7929edde
Deepti Nigam <nigam.d@husky.neu.edu>
2019-04-15 22:11:45 -0400

    Logic added to place order when user clicks a payment button

--- BookMyMovie/src/app/payment/payment.component.ts
+++ BookMyMovie/src/app/payment/payment.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Order_Service} from '../Services/order.service';
+import { order_list } from '../Models/order';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
@@ -7,20 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentComponent implements OnInit {
 
-  constructor() { }
+  orderService : Order_Service;
+ 
+  model:any =  {
+  
+    OrderId: '102',
+    UserId: '232',
+    MovieId: '102',
+  CreatedAt : new Date("2016-05-15"),
+  NoOfSeats: 5,
+  TotalAmount: 750
+    
+  };
 
-  ngOnInit() {
+  
+  @Output() add_pay_invoked = new EventEmitter();
+
+  constructor(orderService : Order_Service) {
+    this.orderService = orderService;}
+
+    ngOnInit() {
+    }
+    onClickPlaceOrder(){
+        alert("inside order ");
+       let neworder$: Observable<order_list> = this.orderService.createOrder(this.model);
+       console.log(this.model);
+       neworder$.subscribe(
+        success => { alert("order success")},
+        error => {
+   //       alert( error);
+        });  
+        this.add_pay_invoked.emit(); 
   }
 
-  log(x)
-  {
-    console.log(x);
-  }
-
-  PlaceOrder(regForm)
+ /* PlaceOrder(regForm)
   {
     console.log(regForm);
+  }*/
+
   }
-}
-
-
+  
