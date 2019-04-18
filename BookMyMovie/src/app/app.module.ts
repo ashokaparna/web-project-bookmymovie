9ad752ca02eccb1c2e7f539e8bafa5fb690fef59
sojitra.d <sojitra.d@husky.neu.edu>
2019-04-18 13:58:15 -0400

    Routing implemented

--- BookMyMovie/src/app/app.module.ts
+++ BookMyMovie/src/app/app.module.ts
@@ -10,6 +10,28 @@ import { FooterComponent } from './footer/footer.component';
 import { CarouselDivComponent } from './carousel-div/carousel-div.component';
 import { MiddleDivComponent } from './middle-div/middle-div.component';
 import { Order_Service } from './Services/order.service';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const appRoutes: Routes = [
+  {
+    path: 'dashboard',
+    component: MiddleDivComponent,
+    data: { title: 'Dashboard' }
+  },
+  {
+    path: 'payment',
+    component: PaymentComponent,
+    data: { title: 'Payment Form' }
+  },
+ 
+  { path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 
 @NgModule({
@@ -19,13 +41,17 @@ import { Order_Service } from './Services/order.service';
     HeaderComponent,
     FooterComponent,
     CarouselDivComponent,
-    MiddleDivComponent
+    MiddleDivComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
-    //AppRoutingModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )   //AppRoutingModule
   ],
   providers: [Order_Service],
   bootstrap: [AppComponent]
