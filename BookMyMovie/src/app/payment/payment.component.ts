2a9ee65b4690facdc29c792c0c714c8c1696169b
KaminiPremPrakash <46829105+KaminiPremPrakash@users.noreply.github.com>
2019-04-20 14:13:13 -0400

    Download PDF feature added

--- BookMyMovie/src/app/payment/payment.component.ts
+++ BookMyMovie/src/app/payment/payment.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { Order_Service} from '../Services/order.service';
+import { Order_Service } from '../Services/order.service';
 import { order_list } from '../Models/order';
 import { Observable } from 'rxjs';
 import { DatePipe } from '@angular/common';
+import * as jsPDF from 'jspdf';
+import * as html2canvas from 'html2canvas';
 // import { getMaxListeners } from 'cluster';
 @Component({
   selector: 'app-payment',
@@ -11,50 +13,60 @@ import { DatePipe } from '@angular/common';
 })
 export class PaymentComponent implements OnInit {
 
-  orderService : Order_Service;
+  orderService: Order_Service;
   //dt = new Date("2016-05-18");
-  
-    //console.log(dateSendingToServer);
- 
 
-  model:any =  {
+  //console.log(dateSendingToServer);
+
+
+  model: any = {
     OrderId: '188888',
     UserId: '2',
     MovieId: '140',
-  CreatedAt : new Date("2016-05-19"),
-  NoOfSeats: 5,
-  TotalAmount: 750,
-  Email: "deepibm2012@gmail.com"
+    CreatedAt: new Date("2016-05-19"),
+    NoOfSeats: 5,
+    TotalAmount: 750,
+    Email: "deepibm2012@gmail.com"
   };
 
-  
+
   @Output() add_pay_invoked = new EventEmitter();
 
-  constructor(orderService : Order_Service) {
-    this.orderService = orderService;}
-
-    ngOnInit() {
-    }
-    onClickPlaceOrder(){
-        alert("inside order ");
-        let dt = new DatePipe('en-US').transform(this.model.CreatedAt, 'dd/MM/yyyy');
-     //   this.model.CreatedAt = dt;
-       let neworder$: Observable<order_list> = this.orderService.createOrder(this.model);
-       
-       //console.log(dt);
-  // console.log( this.model.CreatedAt);
-       neworder$.subscribe(
-        success => { alert("order success")},
-        error => {
-   //       alert( error);
-        });  
-        this.add_pay_invoked.emit(); 
+  constructor(orderService: Order_Service) {
+    this.orderService = orderService;
   }
 
- /* PlaceOrder(regForm)
-  {
-    console.log(regForm);
-  }*/
-
+  ngOnInit() {
   }
-  
+
+  downloadAsPDF() {
+    html2canvas(document.getElementById('col-md-12')).then(function (canvas) {
+      var img = canvas.toDataURL("image/png");
+      var doc = new jsPDF();
+      doc.addImage(img, 'JPEG', 5, 20);
+      doc.save('movieBookingSummary.pdf');
+    });
+  }
+
+  onClickPlaceOrder() {
+    alert("inside order ");
+    let dt = new DatePipe('en-US').transform(this.model.CreatedAt, 'dd/MM/yyyy');
+    //   this.model.CreatedAt = dt;
+    let neworder$: Observable<order_list> = this.orderService.createOrder(this.model);
+
+    //console.log(dt);
+    // console.log( this.model.CreatedAt);
+    neworder$.subscribe(
+      success => { alert("order success") },
+      error => {
+        //       alert( error);
+      });
+    this.add_pay_invoked.emit();
+  }
+
+  /* PlaceOrder(regForm)
+   {
+     console.log(regForm);
+   }*/
+
+}
