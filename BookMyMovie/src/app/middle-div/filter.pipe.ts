82dc758a23a278471fae49b459c311e2a113b0b0
KaminiPremPrakash <46829105+KaminiPremPrakash@users.noreply.github.com>
2019-04-23 18:20:18 -0400

    added code documentation

--- BookMyMovie/src/app/middle-div/filter.pipe.ts
+++ BookMyMovie/src/app/middle-div/filter.pipe.ts
@@ -2,6 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'filter'
 })
+
+// This is to filter the content as per the search string by movie name
 export class FilterPipe implements PipeTransform {
   transform(items: any[], searchText: string): any[] {
     if(!items) return [];
